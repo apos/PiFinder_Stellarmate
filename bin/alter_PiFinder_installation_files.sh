d6ebd49e6db04baa0d3b19d98dd23186f1a9b41e
Axel Pospischil <axel.pospischil@quanto-solutons.de>
2025-05-02 17:45:58 +0200

    make PiFinder Pi5  compatible - SPI display.py

--- bin/alter_PiFinder_installation_files.sh
+++ bin/alter_PiFinder_installation_files.sh
@@ -207,14 +207,10 @@ echo "➡️ Detected Version Combo: $current_pifinder / $current_pi / $current_
 
 if should_apply_patch "2.2.0" "P5" "bookworm"; then
     if ! grep -q 'from luma.core.interface.serial import noop' "$display_py"; then
-        sed -i 's|serial = spi(|serial = spi(gpio=noop(), port=10, |g' "$display_py"
+        sed -i 's|serial = spi(|serial = spi(gpio=noop(), port=10, |' "$display_py"
         echo "✅ Import für noop hinzugefügt"
     fi
-
-    # Ersetze serial = spi(...) durch serial = spi(gpio=gpio_cdev(), ...)
-    sed -i 's|serial = spi(|serial = spi(gpio=noop(), |g' "$display_py"
     echo "✅ Patched all 'serial = spi(...)' calls for Pi5"
-
 else
     echo "⏩ Skipping patch for displays.py: ❌ incompatible version/pi/os"
 fi
