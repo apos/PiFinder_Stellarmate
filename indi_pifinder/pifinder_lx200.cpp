a91c76cf2aed5549db55ebb04c3063acc1809346
Axel Pospischil <apos@blue-it.org>
2025-11-15 17:54:37 +0100

    Still not getting INDI Driver run again after code loss. Recovered an old version

--- indi_pifinder/pifinder_lx200.cpp
+++ indi_pifinder/pifinder_lx200.cpp
@@ -1,184 +1,386 @@
 #include "pifinder_lx200.h"
+
 #include "indicom.h"
+#include "indiproperty.h"
 #include "indilogger.h"
-#include "lx200driver.h"
 
 #include <memory>
-#include <cstring>
-#include <unistd.h>
-#include <cmath>
-#include <mutex>
+#include <string.h>
 
-// We declare an auto pointer to PiFinderLX200.
-std::unique_ptr<PiFinderLX200> pifinderlx200(new PiFinderLX200());
+// We declare an auto pointer to PiFinder.
+std::unique_ptr<PiFinder> pifinder(new PiFinder());
 
-PiFinderLX200::PiFinderLX200() : LX200Telescope()
+PiFinder::PiFinder()
 {
-    // Constructor
+    // Initialize INDI properties
+    // Connection switch
+    ConnectionS[0].name = "CONNECT";
+    ConnectionS[0].s = INDI::ISS_OFF;
+    ConnectionS[1].name = "DISCONNECT";
+    ConnectionS[1].s = INDI::ISS_ON;
+    this->ConnectionSP.np = this->ConnectionS;
+    ConnectionSP.name = "CONNECTION";
+    ConnectionSP.label = "Connection";
+    ConnectionSP.type = INDI::IPS_CS_EQ_CONJ;
+    ConnectionSP.access = INDI::IP_RW;
+    ConnectionSP.state = INDI::IPS_IDLE;
+    ConnectionSP.perm = INDI::IP_RW;
+    ConnectionSP.rule = INDI::ISR_1OF2;
+    ConnectionSP.aux = "Main";
+    ConnectionSP.nnp = 2;
+
+    // Equatorial coordinates
+    EquatorialEODN[0].name = "RA";
+    EquatorialEODN[0].label = "Right Ascension";
+    EquatorialEODN[0].format = "%02.0f:%02.0f:%04.1f";
+    EquatorialEODN[0].min = 0;
+    EquatorialEODN[0].max = 24;
+    EquatorialEODN[0].step = 0;
+    EquatorialEODN[0].value = 0;
+
+    EquatorialEODN[1].name = "DEC";
+    EquatorialEODN[1].label = "Declination";
+    EquatorialEODN[1].format = "%+02.0f:%02.0f:%02.0f";
+    EquatorialEODN[1].min = -90;
+    EquatorialEODN[1].max = 90;
+    EquatorialEODN[1].step = 0;
+    EquatorialEODN[1].value = 0;
+
+    this->EquatorialEODNP.np = this->EquatorialEODN;
+    EquatorialEODNP.name = "EQUATORIAL_EOD_COORD";
+    EquatorialEODNP.label = "RA/DEC J2000";
+    EquatorialEODNP.type = INDI::IP_NUMBER;
+    EquatorialEODNP.access = INDI::IP_RW;
+    EquatorialEODNP.state = INDI::IPS_IDLE;
+    EquatorialEODNP.perm = INDI::IP_RW;
+    EquatorialEODNP.nnp = 2;
+
+    // Horizontal coordinates (for display, not control)
+    HorizontalCoordinatesN[0].name = "ALT";
+    HorizontalCoordinatesN[0].label = "Altitude";
+    HorizontalCoordinatesN[0].format = "%+02.0f:%02.0f:%02.0f";
+    HorizontalCoordinatesN[0].min = -90;
+    HorizontalCoordinatesN[0].max = 90;
+    HorizontalCoordinatesN[0].step = 0;
+    HorizontalCoordinatesN[0].value = 0;
+
+    HorizontalCoordinatesN[1].name = "AZ";
+    HorizontalCoordinatesN[1].label = "Azimuth";
+    HorizontalCoordinatesN[1].format = "%03.0f:%02.0f:%02.0f";
+    HorizontalCoordinatesN[1].min = 0;
+    HorizontalCoordinatesN[1].max = 360;
+    HorizontalCoordinatesN[1].step = 0;
+    HorizontalCoordinatesN[1].value = 0;
+
+    this->HorizontalCoordinatesNP.np = this->HorizontalCoordinatesN;
+    HorizontalCoordinatesNP.name = "HORIZONTAL_COORDINATES";
+    HorizontalCoordinatesNP.label = "Alt/Az";
+    HorizontalCoordinatesNP.type = INDI::IP_NUMBER;
+    HorizontalCoordinatesNP.access = INDI::IP_RO;
+    HorizontalCoordinatesNP.state = INDI::IPS_IDLE;
+    HorizontalCoordinatesNP.perm = INDI::IP_RO;
+    HorizontalCoordinatesNP.nnp = 2;
 }
 
-const char *PiFinderLX200::getDefaultName()
+// Helper function to send a command and get a response
+bool PiFinder::SendCommand(const char *cmd, char *response, int max_len)
 {
-    return "PiFinder LX200";
-}
+    if (pifinder_fd < 0)
+        return false;
 
-bool PiFinderLX200::Handshake()
-{
-    // PiFinder's pos_server.py does not respond to a simple ACK (0x06).
-    // Instead, it expects LX200 commands directly. We'll send a :GR# command
-    // to get the current RA, which will also serve as a handshake.
-#define MAX_RESPONSE_SIZE 80
-#define LX200_TIMEOUT 5 /* FD timeout in seconds */
-
-    char command[] = ":GR#";
-    char response[MAX_RESPONSE_SIZE];
-    int nbytes_write = 0;
-    int nbytes_read = 0;
-    int error_type;
-
-    LOGF_INFO("Sending initial handshake command: %s", command);
-
-    // Use the global mutex for thread-safe serial communication
-    std::unique_lock<std::mutex> guard(lx200CommsLock);
-    if ((error_type = tty_write_string(PortFD, command, &nbytes_write)) != TTY_OK)
+    DEBUGF(INDI::Logger::DBG_COMMAND, "CMD <%s>", cmd);
+    if (write(pifinder_fd, cmd, strlen(cmd)) < 0)
     {
-        LOGF_ERROR("Initial handshake (GR) command failed: %s", command);
-        Disconnect();
+        DEBUGF(INDI::Logger::DBG_ERROR, "Error writing to socket: %s", strerror(errno));
         return false;
     }
 
-    // Wait for a response from the PiFinder
-    if ((error_type = tty_read_section(PortFD, response, '#', LX200_TIMEOUT, &nbytes_read)) != TTY_OK)
+    // Wait a bit for the response
+    usleep(50000);
+
+    int bytes_read = read(pifinder_fd, response, max_len - 1);
+    if (bytes_read > 0)
     {
-        LOG_ERROR("No response to initial handshake (GR) command.");
-        Disconnect();
+        response[bytes_read] = '\0';
+        DEBUGF(INDI::Logger::DBG_COMMAND, "RES <%s>", response);
+    }
+    else
+    {
+        response[0] = '\0';
+        DEBUGF(INDI::Logger::DBG_ERROR, "Error reading from socket");
+    }
+    return bytes_read > 0;
+}
+
+
+// Close the connection
+void PiFinder::Close()
+{
+    if (pifinder_fd >= 0)
+    {
+        shutdown(pifinder_fd, SHUT_RDWR);
+        close(pifinder_fd);
+        pifinder_fd = -1;
+    }
+}
+
+const char *PiFinder::getDefaultName()
+{
+    return "PiFinder";
+}
+
+bool PiFinder::Handshake()
+{
+    struct sockaddr_in server_addr;
+
+    pifinder_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (pifinder_fd < 0)
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "Error creating socket: %s", strerror(errno));
         return false;
     }
 
-    LOGF_INFO("Received response to handshake: %s", response);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(4030);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // If we got a response, assume connection is established.
-    // The actual parsing of RA will happen in ReadScopeStatus.
+    if (connect(pifinder_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        DEBUGF(INDI::Logger::DBG_ERROR, "Error connecting to PiFinder: %s", strerror(errno));
+        Close();
+        return false;
+    }
+
+    DEBUGF(INDI::Logger::DBG_SESSION, "Connected to PiFinder on fd %d", pifinder_fd);
     return true;
 }
 
-bool PiFinderLX200::ReadScopeStatus()
+bool PiFinder::ReadScopeStatus()
 {
-    char command[] = ":GR#"; // Command to get RA
-    char RAResponse[MAX_RESPONSE_SIZE];
-    int nbytes_write = 0;
-    int nbytes_read = 0;
-    int error_type;
+    if (pifinder_fd < 0)
+        return false;
 
-    // Use the global mutex for thread-safe serial communication
-    std::unique_lock<std::mutex> guard(lx200CommsLock);
+    char response[32];
+    int h, m, s, d;
+    double jnow_ra = 0, jnow_dec = 0;
 
     // Get RA
-    if ((error_type = tty_write_string(PortFD, command, &nbytes_write)) != TTY_OK)
+    if (SendCommand(":GR#", response, sizeof(response)))
     {
-        LOGF_ERROR("ReadScopeStatus RA command failed: %s", command);
-        return false;
-    }
-    if ((error_type = tty_read_section(PortFD, RAResponse, '#', LX200_TIMEOUT, &nbytes_read)) != TTY_OK)
-    {
-        LOG_ERROR("No response to ReadScopeStatus RA command.");
-        return false;
+        if (sscanf(response, "%d:%d:%d#", &h, &m, &s) == 3)
+        {
+            jnow_ra = h + m / 60.0 + s / 3600.0;
+        }
     }
 
-    // Get DEC
-    char dec_command[] = ":GD#"; // Command to get DEC
-    char DecResponse[MAX_RESPONSE_SIZE];
-    if ((error_type = tty_write_string(PortFD, dec_command, &nbytes_write)) != TTY_OK)
+    // Get Dec
+    if (SendCommand(":GD#", response, sizeof(response)))
     {
-        LOGF_ERROR("ReadScopeStatus DEC command failed: %s", dec_command);
-        return false;
-    }
-    if ((error_type = tty_read_section(PortFD, DecResponse, '#', LX200_TIMEOUT, &nbytes_read)) != TTY_OK)
-    {
-        LOG_ERROR("No response to ReadScopeStatus DEC command.");
-        return false;
+        if (sscanf(response, "%d*%d'%d#", &d, &m, &s) == 3)
+        {
+            jnow_dec = d + m / 60.0 + s / 3600.0;
+            if (response[0] == '-')
+                jnow_dec = -jnow_dec;
+        }
     }
 
-    // The PiFinder returns strings with a trailing '#', which the standard
-    // INDI LX200 parsing functions cannot handle. We need to strip it.
-    char *hash_ptr = strchr(RAResponse, '#');
-    if (hash_ptr != nullptr)
-        *hash_ptr = '\0';
+    // Convert from JNow to J2000
+    struct ln_equ_posn jnow_coords, j2000_coords;
+    jnow_coords.ra = jnow_ra;
+    jnow_coords.dec = jnow_dec;
 
-    hash_ptr = strchr(DecResponse, '#');
-    if (hash_ptr != nullptr)
-        *hash_ptr = '\0';
+    // Get current Julian date
+    double jd = ln_get_julian_from_sys();
 
-    double ra_val, dec_val;
-    if (f_scansexa(RAResponse, &ra_val) == -1)
-    {
-        LOG_ERROR("Error parsing RA string from mount.");
-        return false;
-    }
-    if (f_scansexa(DecResponse, &dec_val) == -1)
-    {
-        LOG_ERROR("Error parsing DEC string from mount.");
-        return false;
-    }
+    // Precess from JNow to J2000
+    ln_precess_equ(&jnow_coords, jd, LN_JULIAN_DATE_J2000);
+    j2000_coords = jnow_coords;
 
-    NewRaDec(ra_val, dec_val);
+    EquatorialEODN[0].value = j2000_coords.ra;
+    EquatorialEODN[1].value = j2000_coords.dec;
+
+    // Update the property
+    IDSetNumber(&EquatorialEODNP, nullptr);
 
     return true;
 }
 
-bool PiFinderLX200::Goto(double ra, double dec)
+bool PiFinder::initProperties()
 {
-    targetRA  = ra;
-    targetDEC = dec;
-    char RAStr[64] = {0}, DecStr[64] = {0};
-    fs_sexa(RAStr, targetRA, 2, 3600);
-    fs_sexa(DecStr, targetDEC, 2, 3600);
+    // Init properties defined in parent
+    INDI::DefaultDevice::initProperties();
 
-    // Stop any existing motion
-    if (EqNP.getState() == IPS_BUSY)
-    {
-        Abort();
-    }
+    // Tell INDI this is a Telescope
+    this->SetCapability(INDI::TELESCOPE_CAN_GOTO, true);
+    this->SetCapability(INDI::TELESCOPE_CAN_SYNC, true);
+    this->SetCapability(INDI::TELESCOPE_CAN_ABORT, false);
+    this->SetCapability(INDI::TELESCOPE_HAS_ON_BOARD_CLOCK, false);
+    this->SetCapability(INDI::TELESCOPE_HAS_GEOGRAPHIC_LOCATION, false);
 
-    // PiFinder expects a specific sequence of commands
-    // 1. Set RA
-    // 2. Set DEC
-    // The slew is initiated by the DEC command.
+    // We want to manage the connection ourselves
+    this->SetCapability(INDI::CONNECTION_CAP, true);
 
-    char command[64];
-    int nbytes_write = 0;
-    int error_type;
-    int h, m, s, d;
-
-    // Set RA
-    getSexComponents(ra, &h, &m, &s);
-    snprintf(command, sizeof(command), ":Sr%02d:%02d:%02d#", h, m, s);
-    {
-        std::unique_lock<std::mutex> guard(lx200CommsLock);
-        if ((error_type = tty_write_string(PortFD, command, &nbytes_write)) != TTY_OK)
-        {
-            LOGF_ERROR("Goto RA command failed: %s", command);
-            return false;
-        }
-    }
-
-    // Set DEC
-    // Custom format for DEC: ":Sd+DD*MM:SS#"
-    char sign = (dec >= 0) ? '+' : '-';
-    double abs_dec = std::abs(dec);
-    getSexComponents(abs_dec, &d, &m, &s);
-    snprintf(command, sizeof(command), ":Sd%c%02d*%02d:%02d#", sign, d, m, s);
-    {
-        std::unique_lock<std::mutex> guard(lx200CommsLock);
-        if ((error_type = tty_write_string(PortFD, command, &nbytes_write)) != TTY_OK)
-        {
-            LOGF_ERROR("Goto DEC command failed: %s", command);
-            return false;
-        }
-    }
-
-    TrackState = SCOPE_SLEWING;
-    LOGF_INFO("Slewing to RA: %s - DEC: %s", RAStr, DecStr);
+    // Equatorial coordinates
+    this->EquatorialEODNP.fill("EQUATORIAL_EOD_COORD", "RA/DE", "JNow", INDI::IP_RW, 0, 60);
+    this->EquatorialEODN[0].fill("RA", "RA", "%02.0f:%02.0f:%04.1f", 0, 24, 0, 0);
+    this->EquatorialEODN[1].fill("DEC", "Dec", "+%02.0f:%02.0f:%02.0f", -90, 90, 0, 0);
+    
+    // Add the properties to the driver
+    this->defineNumber(&EquatorialEODNP);
 
     return true;
+}
+
+bool PiFinder::updateProperties()
+{
+    // Update properties defined in parent
+    INDI::DefaultDevice::updateProperties();
+
+    if (this->isConnected())
+    {
+        // We are connected, so we are ready to receive commands
+        this->defineText(&HorizontalCoordinatesNP);
+    }
+    else
+    {
+        // We are not connected, so we cannot receive commands
+        this->deleteProperty(HorizontalCoordinatesNP.name);
+    }
+
+    return true;
+}
+
+void PiFinder::ISGetProperties(const char *dev)
+{
+    // Get properties defined in parent
+    INDI::DefaultDevice::ISGetProperties(dev);
+}
+
+bool PiFinder::ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n)
+{
+    // We check if the user is trying to connect or disconnect
+    if (strcmp(name, "CONNECTION") == 0)
+    {
+        // The user is trying to connect or disconnect
+        if (states[0].s == ISS_ON)
+        {
+            // The user is trying to connect
+            if (Handshake())
+            {
+                // We are connected
+                this->ConnectionS[0].s = INDI::ISS_ON;
+                this->ConnectionS[1].s = INDI::ISS_OFF;
+            }
+            else
+            {
+                // We are not connected
+                this->ConnectionS[0].s = INDI::ISS_OFF;
+                this->ConnectionS[1].s = INDI::ISS_ON;
+            }
+        }
+        else
+        {
+            // The user is trying to disconnect
+            Close();
+            this->ConnectionS[0].s = INDI::ISS_OFF;
+            this->ConnectionS[1].s = INDI::ISS_ON;
+        }
+    }
+
+    // We update the CONNECTION property
+    this->ConnectionSP.s = INDI::IPS_OK;
+    this->IDSetSwitch(&ConnectionSP, nullptr);
+    return true;
+}
+
+bool PiFinder::ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n)
+{
+    // We check if the user is trying to set the horizontal coordinates
+    if (strcmp(name, "HORIZONTAL_COORDINATES") == 0)
+    {
+        // The user is trying to set the horizontal coordinates
+        // We parse the coordinates
+        double alt, az;
+        if (sscanf(texts[0], "%lf", &alt) == 1 && sscanf(texts[1], "%lf", &az) == 1)
+        {
+            // We set the coordinates
+            this->HorizontalCoordinatesN[0].value = alt;
+            this->HorizontalCoordinatesN[1].value = az;
+            this->HorizontalCoordinatesNP.s = INDI::IPS_OK;
+        }
+        else
+        {
+            // We cannot parse the coordinates
+            this->HorizontalCoordinatesNP.s = INDI::IPS_ALERT;
+        }
+    }
+
+    // We update the HORIZONTAL_COORDINATES property
+    this->IDSetText(&HorizontalCoordinatesNP, nullptr);
+    return true;
+}
+
+bool PiFinder::ISNewNumber(const char *dev, const char *name, double *values, char *names[], int n)
+{
+    if (strcmp(name, "EQUATORIAL_EOD_COORD") == 0)
+    {
+        // The user is trying to set the equatorial coordinates (GoTo)
+        double ra = values[0];
+        double dec = values[1];
+
+        char command[64];
+        char response[32];
+
+        // Format RA
+        int h = ra;
+        int m = (ra - h) * 60;
+        int s = (ra - h - m / 60.0) * 3600;
+        snprintf(command, sizeof(command), ":Sr%02d:%02d:%02d#", h, m, s);
+        SendCommand(command, response, sizeof(response));
+
+        // Format Dec
+        char sign = dec >= 0 ? '+' : '-';
+        dec = fabs(dec);
+        int d = dec;
+        m = (dec - d) * 60;
+        s = (dec - d - m / 60.0) * 3600;
+        snprintf(command, sizeof(command), ":Sd%c%02d*%02d:%02d#", sign, d, m, s);
+        
+        this->EquatorialEODNP.s = INDI::IPS_BUSY;
+        this->IDSetNumber(&EquatorialEODNP, nullptr);
+
+        if (SendCommand(command, response, sizeof(response)) && response[0] == '1')
+        {
+            this->EquatorialEODNP.s = INDI::IPS_OK;
+            this->EquatorialEODN[0].value = ra;
+            this->EquatorialEODN[1].value = dec;
+        }
+        else
+        {
+            this->EquatorialEODNP.s = INDI::IPS_ALERT;
+        }
+        this->IDSetNumber(&EquatorialEODNP, nullptr);
+        return true;
+    }
+
+    // We check if the user is trying to set the horizontal coordinates
+    if (strcmp(name, "HORIZONTAL_COORDINATES") == 0)
+    {
+        // The user is trying to set the horizontal coordinates
+        // We set the coordinates
+        this->HorizontalCoordinatesN[0].value = values[0];
+        this->HorizontalCoordinatesN[1].value = values[1];
+        this->HorizontalCoordinatesNP.s = INDI::IPS_OK;
+    }
+
+    // We update the HORIZONTAL_COORDINATES property
+    this->IDSetNumber(&HorizontalCoordinatesNP, nullptr);
+    return true;
+}
+
+bool PiFinder::ISNewBLOB(const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[], char *formats[], char *names[], int n)
+{
+    // We call the parent method
+    return INDI::DefaultDevice::ISNewBLOB(dev, name, sizes, blobsizes, blobs, formats, names, n);
 }
